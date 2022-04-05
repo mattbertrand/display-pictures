3e648a795642525753b5f9ea4aab6a7213ddbf06
Bertrand <matthieu.bertrand@accenture.com>
2022-04-05 08:48:09 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { SRLWrapper } from "simple-react-lightbox";
+
+const images = [
+  { id: '1', imageName: '001.jpg'},
+  { id: '2', imageName: '002.jpg'},
+  { id: '3', imageName: '003.jpg'},
+  { id: '4', imageName: '004.jpg'},
+  { id: '5', imageName: '005.jpg'},
+  { id: '6', imageName: '006.jpg'},
+  { id: '7', imageName: '007.jpg'},
+  { id: '8', imageName: '008.jpg'},
+  { id: '9', imageName: '009.jpg'},
+  { id: '10', imageName: '010.jpg'}
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SRLWrapper>
+        <div className='container'>
+          { images.map(image => 
+          <div key={image.id} className="image-card">
+            <a href={`./images/${image.imageName}`}>
+              <img className='image' src={`./images/${image.imageName}`} alt="" />
+            </a>
+            </div>)}
+        </div>
+      </SRLWrapper>
     </div>
   );
 }
